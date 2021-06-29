d1be2e0ca702d40a359376ba26dda9fca4936ce2
Noah Parzentny <noah@parzentny.net>
2021-06-29 16:20:16 +0200

    Added rent-station visibility on rent information

--- user/calendar_inc/rent.calendar.load.php
+++ user/calendar_inc/rent.calendar.load.php
@@ -13,17 +13,38 @@ $query = "SELECT * FROM `rent-list` WHERE ID_Customer = '$id'";
 $statement = $conn->query($query);
 $result = $statement->fetch_all(MYSQLI_ASSOC);
 
+
 foreach ($result as $row) {
-    $data[] = array(
-        'title'   => "Ausleihe Nr.: " . $row["ID_Rent"],
-        'id'   => $row["ID_Rent"],
-        'start'   => $row["start"],
-        'end'   => $row["end"],
-        'tarif'   => getTarif($conn, $row["ID_Tariff"])['name'],
-        'klasse'   => getClass($conn, $row["ID_Class"])['name'],
-        'kunde_id' => $row['ID_Customer'],
-        'price' => $row['price'],
-    );
+    $chassisNumber = $row['chassis_number'];
+    $queryVehicle = "SELECT ID_Station FROM vehicle WHERE chassis_number = '$chassisNumber'";
+    $statementVehicle = $conn->query($queryVehicle);
+    $resultVehicle = $statementVehicle->fetch_all(MYSQLI_ASSOC);
+
+    // var_dump($resultVehicle);
+
+    foreach($resultVehicle as $rowVehicle) {
+        $stationId = $rowVehicle['ID_Station'];
+        $queryStation = "SELECT `rent-station`.street, town.town FROM `rent-station` INNER JOIN town ON town.ZIP = `rent-station`.ZIP WHERE ID_Station = '$stationId'";
+        $statementStation = $conn->query($queryStation);
+        $dataStation = $statementStation->fetch_all();
+        // var_dump($dataStation);
+
+        foreach($dataStation as $rowStation){
+            
+            // var_dump($rowStation);
+            $data[] = array(
+                'title'   => "Ausleihe Nr.: " . $row["ID_Rent"],
+                'id'   => $row["ID_Rent"],
+                'start'   => $row["start"],
+                'end'   => $row["end"],
+                'tarif'   => getTarif($conn, $row["ID_Tariff"])['name'],
+                'klasse'   => getClass($conn, $row["ID_Class"])['name'],
+                'station' => $rowStation['0'] .", " .$rowStation['1'],
+                'price' => $row['price'],
+            );
+        }
+    }
+
 }
 
 echo json_encode($data);
